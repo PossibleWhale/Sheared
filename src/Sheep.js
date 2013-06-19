82acf45da5c696d88ffb41f9f1c576a8208e6d9f
Ashley Fisher <fish.ash@gmail.com>
2013-06-19 16:56:21 -0700

    And here is the actual Sheep file. Derp.

---
+++ src/Sheep.js
@@ -0,0 +1,25 @@
+import ui.ImageView as ImageView;
+
+exports = Class(ImageView, function (supr) {
+    this.init = function (opts) {
+        opts = merge(opts, {
+            image: 'resources/images/16x9/ewe-white.png'
+        });
+
+        supr(this, 'init', [opts]);
+    };
+
+    this.run = function () {
+        setInterval(bind(this, function () {
+            this.style.x = this.style.x - 10;
+            if (this.style.x < 0) {
+                this.removeFromSuperview();
+            }
+        }), 100)
+    };
+});
+
+// return a random integer between 0 and 7, inclusive
+function randomLane () {
+    return Math.floor(Math.random()*8);
+};
