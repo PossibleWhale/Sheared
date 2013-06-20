255bcf319f6ee8aed4f330587a1aee7fe946ea77
Cory Dodt <corydodt@gmail.com>
2013-06-20 12:05:11 -0700

    garment buttons and costs aligned

--- src/constants.js
+++ src/constants.js
@@ -18,6 +18,8 @@ exports = {
 
     TYPE_NAKED: null,
 
+    TYPE_YARN: 
+        {label: 'yarn'},
     TYPE_CAP: 
         {label: 'cap'},
     TYPE_SOCK: 
@@ -25,10 +27,12 @@ exports = {
     TYPE_SCARF: 
         {label: 'scarf'},
     TYPE_MITTEN: 
-        {label: 'scarf'},
+        {label: 'mitten'},
     TYPE_SWEATER: 
-        {label: 'scarf'}
+        {label: 'sweater'}
 };
 exports.colors = [exports.COLOR_WHITE, exports.COLOR_RED, exports.COLOR_GREEN,
     exports.COLOR_BLUE, exports.COLOR_YELLOW, exports.COLOR_BLACK];
 
+exports.garments = [exports.TYPE_YARN, exports.TYPE_CAP, exports.TYPE_SOCK,
+    exports.TYPE_SCARF, exports.TYPE_MITTEN, exports.TYPE_SWEATER];
