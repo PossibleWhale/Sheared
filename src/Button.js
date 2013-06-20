0d5ec74a18f9c982df7992dbf850368f156a4c08
Cory Dodt <corydodt@gmail.com>
2013-06-19 19:53:06 -0700

    fix buttons for scaling

---
+++ src/Button.js
@@ -0,0 +1,26 @@
+/*
+ * A common button
+ */
+
+"use strict";
+
+import ui.View;
+import ui.ImageView;
+import ui.TextView;
+import src.constants as constants;
+
+var colors = constants.colors;
+
+// this is based on TextView because our buttons will be overlaid on an image
+// background anyway
+exports = Class(ui.TextView, function (supr) {
+    var init = function (opts) {
+        if (GC.debug) {
+            merge(opts, {
+                backgroundColor: '#c6c',
+                opacity: 0.7
+            });
+        }
+        supr(this, init, opts);
+    };
+});
