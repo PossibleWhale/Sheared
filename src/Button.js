6fad918820e37e238be774d7c2c1a0bc2debc0ab
Cory Dodt <corydodt@gmail.com>
2013-07-01 20:27:20 -0700

    craftscreen fonts

--- src/Button.js
+++ src/Button.js
@@ -12,9 +12,19 @@ import src.constants as constants;
 var colors = constants.colors;
 
 // this is based on TextView because our buttons will be overlaid on an image
-// background anyway
+// background, anyway
 exports = Class(ui.TextView, function (supr) {
     this.init = function (opts) {
+        var defaults = {
+                size: 128,
+                strokeWidth: 4,
+                strokeColor: '#000000',
+                color: '#ffffff'
+        };
+        merge(defaults, opts); /* merge opts over defaults so the passed-in
+                                * opts take precedence
+                                */
+        opts = defaults;
         if (GC.debug) {
             merge(opts, {
                 backgroundColor: '#c6c',
