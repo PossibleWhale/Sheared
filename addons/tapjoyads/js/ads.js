6f583987618196cb9b63b4da33367ee32ddbce57
Cory Dodt <corydodt@gmail.com>
2013-07-19 15:55:03 -0700

    plugin js should not execute in the browser, which does not support NATIVE

--- addons/tapjoyads/js/ads.js
+++ addons/tapjoyads/js/ads.js
@@ -1,17 +1,20 @@
 
 var TapjoyAds = Class(function () {
-    this.init = function _a_init() {
-        NATIVE.events.registerHandler('tapjoyads', bind(this, function (evt) {
-            console.log('[JS PLUGIN] Response from Tapjoy API: message="' + evt.message + '" code=' + evt.errorCode);
-            var cb = this.callback;
-            cb(evt);
-        }));
-    };
+    if (NATIVE && NATIVE.events) {
+        this.init = function _a_init() {
+            NATIVE.events.registerHandler('tapjoyads', bind(this, function (evt) {
+                console.log('[JS PLUGIN] Response from Tapjoy API: message="' + evt.message + '" code=' + evt.errorCode);
+                var cb = this.callback;
+                cb(evt);
+            }));
+        };
 
-    this.showAd = function _a_showAd(callback) {
-        this.callback = callback;
-        NATIVE.plugins.sendEvent('TapjoyAdsPlugin', 'requestAd', '');
-    };
+        this.showAd = function _a_showAd(callback) {
+            this.callback = callback;
+            NATIVE.plugins.sendEvent('TapjoyAdsPlugin', 'requestAd', '');
+        };
+    }
+
 });
 
 exports = new TapjoyAds();
