23f25dd85efbb635960579b8e62a25040fd4f13a
Cory Dodt <corydodt@gmail.com>
2013-07-28 22:50:29 -0700

    move methods up to parent scope

--- src/Player.js
+++ src/Player.js
@@ -22,73 +22,75 @@ exports = Class(Emitter, function Player(supr) {
 
         this.addCraft = bind(this.crafts, this.crafts.addCraft);
  
-        this.setUpgrades = function () {
-            this.maxClipperHealth = Math.min(10, 5 + Math.max(this.upgrades.get('temp_power').value,
-                                    this.upgrades.get('perm_power').value));
-            this.boltMultiplier = Math.max(this.upgrades.get('temp_mult').value,
-                                    this.upgrades.get('perm_mult').value);
-            this.diamondBlade = this.upgrades.get('temp_diamond').value || 
-                                    this.upgrades.get('perm_diamond').value;
-        };
         this.setUpgrades();
-
-        this.purchased = function (tempOrPerm, upgradeName) {
-            if (upgradeName === 'diamond') {
-                this.upgrades.addToUpgrade('temp_diamond', true);
-                // if a permanent upgrade was purchased then the temporary one was "purchased" too
-                if (tempOrPerm === 'perm') {
-                    this.upgrades.addToUpgrade(tempOrPerm + '_diamond', true);
-                }
-            } else {
-                var key = tempOrPerm + '_' + upgradeName;
-                this.upgrades.addToUpgrade('temp_' + upgradeName, this.upgrades.get('temp_' + upgradeName).value + 1);
-                if (tempOrPerm === 'perm') {
-                    this.upgrades.addToUpgrade(key, this.upgrades.get(key).value + 1);
-                }
-            }
-            this.emit('player:purchased');
-        };
-
-        // add a specified amount of coins to the player's wallet
-        this.addCoins = function (amt) {
-            this.stats.increment('coins', amt);
-            this.stats.increment('coinsEarned', amt);
-        }
 
         // add all the wool from another inventory to this one
         this.mergeWoolCounts = bind(this.wool, this.wool.mergeCounts);
 
-        this.shearedSheep = function (sheep) {
-            if (sheep.isRam) {
-                this.stats.increment('ramsSheared.' + sheep.color.label);
-            } else {
-                this.stats.increment('ewesSheared.' + sheep.color.label);
+    };
+
+    this.setUpgrades = function () {
+        this.maxClipperHealth = Math.min(10, 5 + Math.max(this.upgrades.get('temp_power').value,
+                                this.upgrades.get('perm_power').value));
+        this.boltMultiplier = Math.max(this.upgrades.get('temp_mult').value,
+                                this.upgrades.get('perm_mult').value);
+        this.diamondBlade = this.upgrades.get('temp_diamond').value || 
+                                this.upgrades.get('perm_diamond').value;
+    };
+
+    this.purchased = function (tempOrPerm, upgradeName) {
+        if (upgradeName === 'diamond') {
+            this.upgrades.addToUpgrade('temp_diamond', true);
+            // if a permanent upgrade was purchased then the temporary one was "purchased" too
+            if (tempOrPerm === 'perm') {
+                this.upgrades.addToUpgrade(tempOrPerm + '_diamond', true);
             }
-        };
-
-        this.collectedBattery = function () {
-            this.stats.increment('batteries');
-        };
-
-        this.collectedDiamond = function () {
-            this.stats.increment('diamonds');
-        };
-
-        this.hitWithBlade = function (isDiamond) {
-            if (isDiamond) {
-                this.stats.increment('diamondBladesHit');
-            } else {
-                this.stats.increment('regularBladesHit');
+        } else {
+            var key = tempOrPerm + '_' + upgradeName;
+            this.upgrades.addToUpgrade('temp_' + upgradeName, this.upgrades.get('temp_' + upgradeName).value + 1);
+            if (tempOrPerm === 'perm') {
+                this.upgrades.addToUpgrade(key, this.upgrades.get(key).value + 1);
             }
-        };
+        }
+        this.emit('player:purchased');
+    };
 
-        this.bladeFired = function (isDiamond) {
-            if (isDiamond) {
-                this.stats.increment('diamondBladesFired');
-            } else {
-                this.stats.increment('regularBladesFired');
-            }
-        };
+    // add a specified amount of coins to the player's wallet
+    this.addCoins = function (amt) {
+        this.stats.increment('coins', amt);
+        this.stats.increment('coinsEarned', amt);
+    }
+
+    this.shearedSheep = function (sheep) {
+        if (sheep.isRam) {
+            this.stats.increment('ramsSheared.' + sheep.color.label);
+        } else {
+            this.stats.increment('ewesSheared.' + sheep.color.label);
+        }
+    };
+
+    this.collectedBattery = function () {
+        this.stats.increment('batteries');
+    };
+
+    this.collectedDiamond = function () {
+        this.stats.increment('diamonds');
+    };
+
+    this.hitWithBlade = function (isDiamond) {
+        if (isDiamond) {
+            this.stats.increment('diamondBladesHit');
+        } else {
+            this.stats.increment('regularBladesHit');
+        }
+    };
+
+    this.bladeFired = function (isDiamond) {
+        if (isDiamond) {
+            this.stats.increment('diamondBladesFired');
+        } else {
+            this.stats.increment('regularBladesFired');
+        }
     };
 
     // true if the player has sufficient wool to craft something
