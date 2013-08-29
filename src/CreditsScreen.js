bc37e72092de02704b359d3c10e860bc707f245e
Ashley Fisher <fish.ash@gmail.com>
2013-08-28 18:06:35 -0700

    credits screen fixed

--- src/CreditsScreen.js
+++ src/CreditsScreen.js
@@ -1,5 +1,6 @@
 import ui.ImageView as ImageView;
 import src.util as util;
+import src.MuteButton as MuteButton;
 
 exports = Class(ImageView, function (supr) {
     this.init = function (opts) {
@@ -13,6 +14,66 @@ exports = Class(ImageView, function (supr) {
     };
 
     this.build = function() {
+        // header
+        this.addSubview(new ImageView({
+            x: 0,
+            y: 0,
+            width: 1024,
+            height: 80,
+            image: 'resources/images/background-header-wood.png'
+        }));
+
+        // footer
+        this.addSubview(new ImageView({
+            x: 0,
+            y: 496,
+            width: 1024,
+            height: 80,
+            image: 'resources/images/background-footer-wood.png'
+        }));
+
+        // title
+        this.addSubview(new ImageView({
+            x: 192,
+            y: 0,
+            width: 640,
+            height: 80,
+            image: 'resources/images/header-credits.png'
+        }));
+
+        // background
+        this.addSubview(new ImageView({
+            x: 0,
+            y: 80,
+            width: 1024,
+            height: 416,
+            image: 'resources/images/background-wood.png'
+        }));
+
+        this.addSubview(new ImageView({
+            x: 0,
+            y: 80,
+            width: 1024,
+            height: 416,
+            image: 'resources/images/tab-0.png'
+        }));
+
+        this.addSubview(new ImageView({
+            x: 172,
+            y: 113,
+            width: 680,
+            height: 350,
+            image: 'resources/images/credits.png'
+        }));
+
+        this.addSubview(new MuteButton({
+            x: 944,
+            y: 0,
+            zIndex: 9999,
+            width: 80,
+            height: 80
+        }));
+
         var backButton = new ImageView({
             superview: this,
             x: 0,
