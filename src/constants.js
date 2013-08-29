3a16aebf7542700cbff929b285587f0dc2778ec1
Nathan Cooper <nathanpcooper@gmail.com>
2013-08-29 11:49:44 -0700

    Adjusted store upgrades prices

--- src/constants.js
+++ src/constants.js
@@ -124,9 +124,9 @@ exports = {
     ramRarity: 0.2,
 
     UPGRADE_PRICES: {
-        power: [250, 500,  1000, 2000, 4000],
-        mult:  [500, 1000, 2000, 4000, 8000],
-        blade: [750, 1500, 3000, 6000, 12000],
+        power: [1000, 2000, 3000, 4000,  5000],
+        mult:  [3000, 6000, 9000, 12000, 15000],
+        blade: [2000, 4000, 6000, 8000,  10000],
         diamond: 24000,
 
         // wool prices. for now they are all the same... we might want them to be different?
@@ -154,7 +154,7 @@ exports = {
 
     // amount of coins you get from purchasing
     EWEROS_QUANTITIES: [
-        8000, 16000, 24000, 48000, 72000 
+        10000, 20000, 40000, 60000, 80000 
     ],
 
     ADS_PRICE: [1],
